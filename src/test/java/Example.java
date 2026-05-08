736691cbcb9f53ea4eadae561b26e9e339b21403
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-05-08 23:07:00 +0200

    cleaning up a bit.

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,6 +1,8 @@
 import jakarta.xml.bind.JAXB;
+import static java.lang.ScopedValue.where;
+import static java.nio.file.Files.newOutputStream;
 import nl.npo.metadatahub.client.Configuration;
-import nl.npo.metadatahub.client.sparql.MetadataSparqlClient;
+import static nl.npo.metadatahub.client.sparql.MetadataSparqlClient.onQueryExecuted;
 import nl.npo.metadatahub.poms.*;
 import nl.vpro.api.client.frontend.NpoApiClients;
 import nl.vpro.domain.media.Channel;
@@ -8,7 +10,7 @@ import nl.vpro.logging.log4j2.CaptureToSimpleLogger;
 import nl.vpro.logging.simple.OutputStreamSimpleLogger;
 import nl.vpro.util.Env;
 import nl.vpro.util.ThreadPools;
-import org.apache.jena.query.ResultSetFormatter;
+import static org.apache.jena.query.ResultSetFormatter.*;
 import org.apache.logging.log4j.jul.Log4jBridgeHandler;
 
 /**
@@ -29,11 +31,9 @@ void main() throws Exception {
         var pomsServices = NpoApiClients.configured(Env.PROD).build();
         var metadataHubMediaService = new MetadataHubMediaService(new Configuration().createClient());
         ) {
-
         for (Day day : List.of(new Day(Channel.NED2, LocalDate.of(2026, 4, 1)), new Day(Channel.NED1, LocalDate.of(2026, 3, 1)))) {
 
-
-            // todo, I couldn get this working yet via MH
+            // todo, I couldn't get this working yet via MH
             var resultFromPoms = pomsServices.getScheduleService().listChannel(day.channel.name(), day.day, null, null, "all", null, 0L, 240);
 
             Path results = Paths.get("results").resolve(day.day().toString()).resolve(day.channel.name());
@@ -43,7 +43,7 @@ void main() throws Exception {
                 var mo = event.getParent();
                 var mid = mo.getMid();
                 var pomsFile = results.resolve(mid + ".poms.xml");
-                try (FileOutputStream out = new FileOutputStream(pomsFile.toFile())) {
+                try (OutputStream out = newOutputStream(pomsFile)) {
                     JAXB.marshal(mo, out);
                     IO.println("POMS: " + pomsFile.toAbsolutePath());
                 }
@@ -51,14 +51,15 @@ void main() throws Exception {
                 var mhJsonFile = results.resolve(mid + ".mh.json");
 
                 try (
-                    FileOutputStream log = new FileOutputStream(results.resolve(mid + ".log").toFile());
-                    CaptureToSimpleLogger capture = CaptureToSimpleLogger.of(OutputStreamSimpleLogger.builder()
-                        .output(log).build());
-                    FileOutputStream jsout = new FileOutputStream(results.resolve(mid + ".mh.json").toFile());
-                    FileOutputStream out = new FileOutputStream(results.resolve(mid + ".mh.xml").toFile())) {
-                    ScopedValue.where(MetadataSparqlClient.onQueryExecuted, (rs) -> {
-                        ResultSetFormatter.outputAsJSON(jsout, rs);
-                    }).run(() -> {
+                    var log = newOutputStream(results.resolve(mid + ".log"));
+                    var capture = CaptureToSimpleLogger.of(
+                        OutputStreamSimpleLogger.builder().output(log).build()
+                    );
+                    var jsout = newOutputStream(results.resolve(mid + ".mh.json"));
+                    var out = newOutputStream(results.resolve(mid + ".mh.xml"))) {
+                    where(onQueryExecuted,
+                        (rs) -> outputAsJSON(jsout, rs)
+                    ).run(() -> {
                         try {
                             metadataHubMediaService.getProgram(mo.getMid()).ifPresentOrElse(
                                 program -> {
@@ -70,10 +71,8 @@ void main() throws Exception {
                         } catch (IllegalStateException e) {
                             IO.println("MH: error fetching program for mid " + mo.getMid() + ": " + e.getMessage());
                         }
-
                     });
                 }
-
             }
         }
 
